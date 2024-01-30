505fccb796a6e0be54fcf98cc6f57e3f0eab2c9f
VladRidun <ridunvv@gmail.com>
2024-01-30 11:08:17 +0400

    1. ToMany [#301848]

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -1,15 +1,21 @@
 package ru.job4j.cars.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "auto_user")
 @Data
+@Builder(builderMethodName = "of")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
     private int id;
     @Column(name = "login")
     private String login;
