04a9e57b5b95ad954e43b08ea6fe443875a64388
VladRidun <ridunvv@gmail.com>
2024-02-16 18:22:10 +0400

    6. Модели и связи. Машины и владельцы [delete user_id from owner table]

--- db/scripts/008_ddl_create_owner_table.sql
+++ db/scripts/008_ddl_create_owner_table.sql
@@ -1,6 +1,5 @@
 create table owner
 (
     id    SERIAL PRIMARY KEY,
-    name   TEXT,
-    user_id  int not null unique references auto_user(id)
+    name   TEXT
 );
--- src/main/java/ru/job4j/cars/model/Owner.java
+++ src/main/java/ru/job4j/cars/model/Owner.java
@@ -17,6 +17,4 @@ public class Owner {
 
     private String name;
 
-    private int userId;
-
 }
